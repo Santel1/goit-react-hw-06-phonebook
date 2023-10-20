496dc230fd338c776bca99b7081cc1efec528776
Santel1 <126976920+Santel1@users.noreply.github.com>
2023-10-20 19:08:18 +0200

    hw-06

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -3,6 +3,11 @@ export const ContactItem = props => {
   return (
     <div className={css.contactItemContainer}>
       <li className={css.contactItem}>
+        <img
+          className={css.contactImage}
+          src="https://icons.veryicon.com/png/o/education-technology/technology-big-data-visualization/user-149.png"
+          alt="user-logo"
+        />
         <span className={css.contactItemText}>{props.name}</span>
         <span className={css.contactItemText}>{props.number}</span>
         <button
